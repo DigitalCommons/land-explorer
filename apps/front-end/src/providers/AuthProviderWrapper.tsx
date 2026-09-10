c433aa6e8cfae1803db214ca5b2fa409ff594bfc
laura <laurasoftware21@gmail.com>
2026-09-10 16:36:08 +0100

    Create auth provider wrapper and wrap app in it

---
+++ apps/front-end/src/providers/AuthProviderWrapper.tsx
@@ -0,0 +1,22 @@
+import { AuthProvider } from "@/components/auth/auth-provider";
+import { authClient } from "@/lib/auth/auth-client";
+import { PropsWithChildren } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+
+export default function AuthProviderWrapper({children}: PropsWithChildren) {
+    const navigate = useNavigate();
+
+    return (
+        <AuthProvider
+            authClient={authClient}
+            redirectTo="/settings/account"
+            socialProviders={["google", "github"]}
+            emailAndPassword={{ requireEmailVerification: true }}
+            navigate={({ to, replace }) => navigate(to, {replace: replace})}
+            Link={({ href, ...props }) => <Link to={href} {...props} />}
+        >
+            {children}
+        </AuthProvider>
+    )
+}
