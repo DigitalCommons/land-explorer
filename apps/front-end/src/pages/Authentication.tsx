2ad5bf20a42425faaf63267690aecb285f25d062
laura <laurasoftware21@gmail.com>
2026-09-10 09:46:28 +0100

    put legacy routes behind ff

--- apps/front-end/src/pages/Authentication.tsx
+++ apps/front-end/src/pages/Authentication.tsx
@@ -5,7 +5,8 @@ import Register from "./Register/Register";
 import Login from "./Login";
 import ResetPassword from "./ResetPassword";
 import FourOhFour from "./FourOhFour";
-import BackgroundImage from "../components/common/BackgroundImage";
+import BackgroundImage from '../components/common/BackgroundImage';
+import constants from "@/constants";
 
 const Authentication = () => {
   const [image, setImage] = useState(0);
@@ -14,24 +15,18 @@ const Authentication = () => {
     setImage(n);
   };
 
-  return (
-    <div>
-      <TopBar limited={true} />
-      <BackgroundImage image={image} />
-      <Routes>
-        <Route path="/" element={<Login updateBgImage={updateBgImage} />} />
-        <Route
-          path="/register"
-          element={<Register updateBgImage={updateBgImage} />}
-        />
-        <Route
-          path="/reset-password"
-          element={<ResetPassword updateBgImage={updateBgImage} />}
-        />
-        <Route path="/*" element={<FourOhFour />} />
-      </Routes>
-    </div>
-  );
-};
+    return (
+        <div>
+            <TopBar limited={true} />
+            <BackgroundImage image={image} />
+            <Routes>
+                {!constants.VITE_FEATURE_USE_BETTERAUTH ? <Route path="/" element={<Login updateBgImage={updateBgImage} />} /> : null }
+                <Route path="/register" element={<Register updateBgImage={updateBgImage} />} />
+                {!constants.VITE_FEATURE_USE_BETTERAUTH ? <Route path="/reset-password" element={<ResetPassword updateBgImage={updateBgImage} />} /> : null }
+                <Route path="/*" element={<FourOhFour />} />
+            </Routes>
+        </div>
+    );
+}
 
 export default Authentication;
