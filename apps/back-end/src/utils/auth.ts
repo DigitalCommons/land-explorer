dab143926c10ef29cf0216beb0b235622247be93
laura <laurasoftware21@gmail.com>
2026-09-10 12:49:50 +0100

    add migration of tables

--- apps/back-end/src/utils/auth.ts
+++ apps/back-end/src/utils/auth.ts
@@ -4,15 +4,23 @@ import { createPool } from "mysql2/promise";
 
 export const auth = betterAuth({
    database: createPool({
-    host: "localhost",
+    host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,    
+    password: process.env.DATABASE_PASSWORD,
     timezone: "Z", // Important to ensure consistent timezone values
+    port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 3306
   }),
   advanced: {
     database: {
       joins: true,
     },
   },
+  emailVerification: {
+    
+  },
+  user: {
+    modelName: "auth_user"
+  }
+  
 });
