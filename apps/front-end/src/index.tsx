3adeb1c85393fa80b6c3789955cf1907202236a3
laura <laurasoftware21@gmail.com>
2026-09-10 16:36:08 +0100

    Create auth provider wrapper and wrap app in it

--- apps/front-end/src/index.tsx
+++ apps/front-end/src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes, useNavigate } from "react-router-dom";
 
 import MapApp from "./pages/MapApp";
 import MyAccount from "./pages/MyAccount";
@@ -17,23 +17,33 @@ import "./index.css";
 import "./assets/styles/style.scss";
 import { TooltipProvider } from "./components/ui/tooltip";
 import constants from "./constants";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProviderWrapper from "./providers/AuthProviderWrapper";
 
 initializeMixpanel();
 
+// Create a client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <TooltipProvider>
-      <BrowserRouter>
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-          <Routes>
-            <Route path="/app" element={<MapApp />} />
-            {!constants.VITE_FEATURE_USE_BETTERAUTH ? <Route path="/app/my-account/*" element={<MyAccount />} /> : null }
-            <Route path="/auth/*" element={<Authentication />} />
-            <Route path="/" element={<Navigate to="/app" replace={true} />} />
-            <Route path="*" element={<FourOhFour />} />
-          </Routes>
-        </ErrorBoundary>
-      </BrowserRouter>
-    </TooltipProvider>
-  </Provider>,
+  <QueryClientProvider client={queryClient}>    
+      <Provider store={store}>
+        <TooltipProvider>
+          <BrowserRouter>
+            <AuthProviderWrapper>
+              <ErrorBoundary FallbackComponent={ErrorFallback}>
+                <Routes>
+                  <Route path="/app" element={<MapApp />} />
+                  {!constants.VITE_FEATURE_USE_BETTERAUTH ? <Route path="/app/my-account/*" element={<MyAccount />} /> : null }
+                  <Route path="/auth/*" element={<Authentication />} />
+                  <Route path="/" element={<Navigate to="/app" replace={true} />} />
+                  <Route path="*" element={<FourOhFour />} />
+                </Routes>
+              </ErrorBoundary>
+            </AuthProviderWrapper>
+          </BrowserRouter>
+        </TooltipProvider>
+      </Provider>    
+    
+  </QueryClientProvider>,
 );
