3e8521acb8bb1b2db979bb49f691c1396d5c4c4f
laura <laurasoftware21@gmail.com>
2026-09-22 12:28:20 +0100

    if we get a 401 from the backend, redirect to auth

--- apps/front-end/src/lib/auth/auth-client.ts
+++ apps/front-end/src/lib/auth/auth-client.ts
@@ -1,10 +1,9 @@
 import { createAuthClient } from "better-auth/react"
-
 export const authClient = createAuthClient({         
     fetchOptions: {
         onError: (ctx) => {
             if (ctx.response.status === 401) {
-                window.location.href = "/auth"
+            window.location.href = "/auth"
         }},             
     }
 })
