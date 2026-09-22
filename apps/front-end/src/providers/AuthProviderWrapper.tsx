e9a93189e7ea91e29e7c82a3c28a8f010084c492
laura <laurasoftware21@gmail.com>
2026-09-22 09:48:19 +0100

    add the login form

--- apps/front-end/src/providers/AuthProviderWrapper.tsx
+++ apps/front-end/src/providers/AuthProviderWrapper.tsx
@@ -13,12 +13,12 @@ export default function AuthProviderWrapper({children}: PropsWithChildren) {
             redirectTo="/app"            
             emailAndPassword={{ minPasswordLength: 6, rememberMe: true, requireEmailVerification: true }}
             viewPaths={{auth: {
-                signUp: "/register",
-                signIn: "/",
+                signUp: "register",
+                signIn: "",
                 //forgotPassword: ""
                 //resetPassword: ""
                 //signOut: ""
-                verifyEmail: "/verify"
+                verifyEmail: "verify"
             }}}
             navigate={({ to, replace }) => navigate(to, {replace: replace})}
             Link={({ href, ...props }) => <Link to={href} {...props} />}
