ef74b823b8a8c14380661ad141dec1620a7b82b8
laura <laurasoftware21@gmail.com>
2026-09-23 11:41:52 +0100

    Update the login page to redirect to app if you are logged in

--- apps/front-end/src/pages/Login.tsx
+++ apps/front-end/src/pages/Login.tsx
@@ -1,16 +1,25 @@
 import { SignIn } from "@/components/auth/sign-in";
+import { Spinner } from "@/components/ui/spinner";
+import { authClient } from "@/lib/auth/auth-client";
+import { useSession } from "@better-auth-ui/react";
 import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
 
 type LoginProps = { updateBgImage: (n: number) => void };
 
 export default function Login({ updateBgImage }: LoginProps) {
+    const {data: session, isFetching} = useSession(authClient);
+
     useEffect(() => {
         updateBgImage(0);
     }, []);
+    
+    if (!session && isFetching) return <Spinner />
+    if (session) return <Navigate to="/app" replace />
 
-    return (        
+    return (
     <div className="relative flex grow justify-center items-center">
-        <SignIn/>        
-    </div>    
+        <SignIn/>
+    </div>
     )
 }
