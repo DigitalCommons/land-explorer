c92bf7ee7c5d49c04e4c7bf97cc2f5bd498a6a27
laura <laurasoftware21@gmail.com>
2026-09-23 11:43:35 +0100

    if we get a 401 back from a call, redirect to login

--- apps/front-end/src/lib/auth/auth-client.ts
+++ apps/front-end/src/lib/auth/auth-client.ts
@@ -1,3 +1,10 @@
 import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({     
+
+export const authClient = createAuthClient({         
+    fetchOptions: {
+        onError: (ctx) => {
+            if (ctx.response.status === 401) {
+                window.location.href = "/auth"
+        }},             
+    }
 })
